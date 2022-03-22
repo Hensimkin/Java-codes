948e0d3136408fd250ca991fc75bb37d98eb2b20
Hen Simkin <hensi1@ac.sce.ac.il>
2022-03-22 19:05:39 +0200

    lion added

--- Homework1/src/animal/Lion.java
+++ Homework1/src/animal/Lion.java
@@ -2,17 +2,18 @@ package animal;
 import diet.Carnivore;
 import food.IEdible;
 import mobility.Point;
+import diet.IDiet;
 
 public class Lion extends Animal
 {
 	final static Point start=new Point(20,0);
-	final static IDiet Meat=new Carnivore()
+	final static IDiet Meat=new Carnivore();
 	private int scarCount=0;
 	
 	public Lion(String name)
 	{
-		super(name,new Point(20,0));
-		super.setWeight();
+		super(name,start);
+		super.setWeight(408.2);
 		
 	}
 	
@@ -26,7 +27,7 @@ public class Lion extends Animal
 		
 	}
 	
-	Animal.
+
 	
 	
 
