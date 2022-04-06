ba46d94974c3aadc223206e5773b1f18093ec2c0
Adir <adirmelker1@gmail.com>
2022-04-06 22:04:51 +0300

    menu added

--- Homework1/src/graphics/ZooFrame.java
+++ Homework1/src/graphics/ZooFrame.java
@@ -14,31 +14,29 @@ public class ZooFrame extends JFrame {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Zoo");
-		//JLabel label = new JLabel("Hello World");
-		JButton file=new JButton();
-		JButton background=new JButton();
-		JButton help=new JButton();
-		frame.setSize(800,600);//frame size
-		file.setText("File");//set buttom name
-		background.setText("background");
-		help.setText("help");
-		file.setSize(100,100);//butoom size
-		background.setSize(100,100);
-		help.setSize(100,100);
-		file.setBounds(0,0,100,100);// buttom bounds
-		background.setBounds(100,0,100,100);
-		help.setBounds(200,0,100,100);
-		file.setVisible(true);//buttom visible
-		background.setVisible(true);
-		help.setVisible(true);
-
-		frame.add(file);
-		frame.add(background);
-		frame.add(help);
-		//frame.getContentPane().add(label);
+		frame.setSize(800,600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		JMenuBar mb=new JMenuBar();
+		frame.setJMenuBar(mb);
+		JMenu file=new JMenu("File");
+		mb.add(file);
+		JMenu background=new JMenu("Background");
+		mb.add(background);
+		JMenu help=new JMenu("Help");
+		mb.add(help);
+		JMenuItem exit=new JMenuItem("Exit");
+		file.add(exit);
+		JMenuItem image=new JMenuItem("Image");
+		background.add(image);
+		JMenuItem green=new JMenuItem("Green");
+		background.add(green);
+		JMenuItem none=new JMenuItem("None");
+		background.add(none);
+		JMenuItem help2=new JMenuItem("Help");
+		help.add(help2);
 
 	}
 
