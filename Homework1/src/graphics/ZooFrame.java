3800be7f85666372e27e757f0b608c80ffae2ae2
Adir <adirmelker1@gmail.com>
2022-04-06 20:33:22 +0300

    zoo frame

--- Homework1/src/graphics/ZooFrame.java
+++ Homework1/src/graphics/ZooFrame.java
@@ -13,8 +13,13 @@ import zoo.*;
 public class ZooFrame extends JFrame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		JFrame frame = new JFrame("Zoo");
+		final JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+
 	}
 
 }
