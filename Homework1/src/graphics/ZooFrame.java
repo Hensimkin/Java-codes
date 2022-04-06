26e3432449f4bcfff66ac9d37baf8a8813e8f93f
Adir <adirmelker1@gmail.com>
2022-04-06 21:23:53 +0300

    ff

--- Homework1/src/graphics/ZooFrame.java
+++ Homework1/src/graphics/ZooFrame.java
@@ -20,12 +20,12 @@ public class ZooFrame extends JFrame {
 		JButton help=new JButton();
 		frame.setSize(800,600);//frame size
 		file.setText("File");//set buttom name
-		file.setText("background");
-		file.setText("help");
+		background.setText("background");
+		help.setText("help");
 		file.setSize(100,100);//butoom size
 		background.setSize(100,100);
 		help.setSize(100,100);
-		file.setBounds(0,0,100,100);
+		file.setBounds(0,0,100,100);// buttom bounds
 		background.setBounds(100,0,100,100);
 		help.setBounds(200,0,100,100);
 		file.setVisible(true);//buttom visible
@@ -33,6 +33,8 @@ public class ZooFrame extends JFrame {
 		help.setVisible(true);
 
 		frame.add(file);
+		frame.add(background);
+		frame.add(help);
 		//frame.getContentPane().add(label);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
