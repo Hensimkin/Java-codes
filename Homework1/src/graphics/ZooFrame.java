ac7e3b2f6090583492c6d43c0115de0e17d810a3
Adir <adirmelker1@gmail.com>
2022-04-07 11:30:42 +0300

    ds

--- Homework1/src/graphics/ZooFrame.java
+++ Homework1/src/graphics/ZooFrame.java
@@ -17,6 +17,8 @@ public class ZooFrame extends JFrame {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Zoo");
+		JLabel label=new JLabel();
+		frame.add(label);
 		frame.setSize(800,600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//frame.pack();
@@ -32,12 +34,37 @@ public class ZooFrame extends JFrame {
 		menuBar.add(help);
 		JMenuItem exit=new JMenuItem("Exit");
 		file.add(exit);
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});	
 		JMenuItem image=new JMenuItem("Image");
 		background.add(image);
+		//exit.addActionListener(new ActionListener() {
+			//@Override
+			//public void actionPerformed(ActionEvent e) {
+				
+			//}
+		//});
 		JMenuItem green=new JMenuItem("Green");
 		background.add(green);
+		green.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				label.setBackground(Color.GREEN);
+				label.setOpaque(true);
+			}
+		});
 		JMenuItem none=new JMenuItem("None");
 		background.add(none);
+		none.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				label.setOpaque(false);
+			}
+		});
 		JMenuItem help2=new JMenuItem("Help");
 		help.add(help2);
 		help2.addActionListener(new ActionListener() {
@@ -45,10 +72,8 @@ public class ZooFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFrame message = new JFrame("Message");
-				final JLabel homework = new JLabel("Home Work 2");
-				final JLabel gu=new JLabel("GUI");
+				final JLabel homework = new JLabel("Home Work 2\n GUI");
 				message.getContentPane().add(homework);
-				message.getContentPane().add(gu);
 				message.setSize(300,300);
 				message.setVisible(true);
 				
