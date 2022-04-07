52a4214bdc7d7cb8d8a57a659fbf3444a91831e8
Hen Simkin <hensi1@ac.sce.ac.il>
2022-04-07 11:28:57 +0300

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/Hensimkin/Java-codes.git

--- Homework1/src/graphics/ZooFrame.java
+++ Homework1/src/graphics/ZooFrame.java
@@ -21,24 +21,25 @@ public class ZooFrame extends JFrame {
 		frame.setSize(800,600);//frame size
 		file.setText("File");//set buttom name
 		background.setText("background");
-		help.setText("help");
+		//help.setText("help");
 		file.setSize(100,100);//butoom size
 		background.setSize(100,100);
-		help.setSize(100,100);
+		//help.setSize(100,100);
 		file.setBounds(0,0,100,100);// buttom bounds
 		background.setBounds(100,0,100,100);
-		help.setBounds(200,0,100,100);
+		//help.setBounds(200,0,100,100);
 		file.setVisible(true);//buttom visible
 		background.setVisible(true);
-		help.setVisible(true);
+		//help.setVisible(true);
 
 		frame.add(file);
 		frame.add(background);
-		frame.add(help);
+		//frame.add(help);
 		//frame.getContentPane().add(label);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
+		//frame.pack();
 		frame.setVisible(true);
+		
 
 	}
 
