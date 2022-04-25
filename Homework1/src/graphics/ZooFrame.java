3b99c35d59e7948476434f221506c7b117ca856b
Adir <adirmelker1@gmail.com>
2022-04-25 22:51:39 +0300

    new zooframe

--- Homework1/src/graphics/ZooFrame.java
+++ Homework1/src/graphics/ZooFrame.java
@@ -21,7 +21,7 @@ import zoo.*;
  * @author Adir Melker 316614569 and Hen Simkin 208514109
  *
  */
-public class ZooFrame extends JFrame
+public class ZooFrame extends JFrame implements ActionListener
 {
 	private static final String BACKGROUND_PATH="C:\\Users\\hanig\\Desktop\\assignment2_pictures";
 	static JFrame frame;
@@ -29,9 +29,23 @@ public class ZooFrame extends JFrame
 	static BufferedImage pic2=null;
 	static ImageIcon pic3;
 	static Image pic4;
-	public static void main(String[] args) 
+	private JMenu file;
+	private JMenu background;
+	private JMenu help;
+	private JMenuItem exit;
+	private JMenuItem green;
+	private JMenuItem none;
+	private JMenuItem image;
+	private JMenuItem help2;
+
+	
+	public static void main(String[] args)
 	{
-		
+		ZooFrame n=new ZooFrame();
+	}
+	
+	public ZooFrame()
+	{
 		ZooPanel1 m=new ZooPanel1();
 		//ZooPanel22 m2=new ZooPanel22();
 		frame = new JFrame("Zoo");
@@ -48,130 +62,106 @@ public class ZooFrame extends JFrame
 		menuBar.add(file);
 		JMenu background=new JMenu("Background");
 		menuBar.add(background);
-		JMenu help=new JMenu("Help");
+		help=new JMenu("Help");
 		menuBar.add(help);
-		JMenuItem exit=new JMenuItem("Exit");
+		exit=new JMenuItem("Exit");
 		file.add(exit);
-		exit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});	
+		exit.addActionListener((ActionListener) this);
 		JMenuItem image=new JMenuItem("Image");
 		background.add(image);
-		image.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e) {
-				/*
-				try
-				{
-					
-					
-					frame.getContentPane().setBackground(null);
-		            pic2 = ImageIO.read(new File("savanna.png"));
-		            label.setBounds(0,0,800,600);
-		            Image pic4 = pic2.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH);
-		            pic3 = new ImageIcon(pic4);
-		            label.setIcon(pic3);
-		            frame.getContentPane().add(label);
-		            
-				}
-			
-				catch(IOException a)
-				{
-					System.out.println("Cant load picture");
-				}
-				*/
-				
-			}
-		
-		});
-		JMenuItem green=new JMenuItem("Green");
+		image.addActionListener((ActionListener) this);
+		green=new JMenuItem("Green");
 		background.add(green);
-		green.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				//ZooPanel m2=new ZooPanel();
-				/*
-				m.setGreen();
-				m.repaint();
-				m.setVisible(true);
-				//frame.add(m2);
-				frame.setVisible(true);
-				*/
-				frame.remove(label);
-				frame.getContentPane().setBackground(Color.green);
-			}
-		});
-		JMenuItem none=new JMenuItem("None");
+		green.addActionListener((ActionListener) this);
+		none=new JMenuItem("None");
 		background.add(none);
-		none.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				/*
-				m.setNatural();
-				m.repaint();
-				m.setVisible(true);
-				frame.setVisible(true);
-				*/
-				frame.remove(label);
-				frame.getContentPane().setBackground(null);
-			}
-		});
-		JMenuItem help2=new JMenuItem("Help");
+		none.addActionListener((ActionListener) this);
+		help2=new JMenuItem("Help");
 		help.add(help2);
-		help2.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				JFrame frameMessage=new JFrame("Message");
-				JLabel labelWork2=new JLabel("<html>Home Work 2<br/>GUI</html>");
-				frameMessage.setSize(300,300);
-				frameMessage.setVisible(true);
-				frameMessage.add(labelWork2);
-				ImageIcon aboutIcon=new ImageIcon("about.jpeg");
-				labelWork2.setIcon(aboutIcon);
-				JButton btOk=new JButton("OK");
-				btOk.setFocusable(false);
-				btOk.setBounds(150,150,70,30);
-				labelWork2.add(btOk);	
-				btOk.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e)
-					{  
-						frameMessage.dispose();
-				    }
-				});
-			}
-		});
+		help2.addActionListener((ActionListener) this);		
 		
 	}
 	
-	
-	
-	public void paintComponent(Graphics g)
+	public void actionPerformed(ActionEvent e) 
 	{
-		super.paintComponents(g);
-		if(pic2!=null)
+		if (e.getSource()==exit)
 		{
-			g.setColor(null);
-			g.drawImage(pic2,0,0,getWidth(),getHeight(), this);
+			System.exit(0);
+
 		}
-	}
-	
-	 
-	 
-	 public void  setpic(String pic1)
+		
+		if (e.getSource()==image)
 		{
+			/*
 			try
 			{
-				pic2 = ImageIO.read(new File(BACKGROUND_PATH+"//"+pic1)); 
+				
+				
+				frame.getContentPane().setBackground(null);
+	            pic2 = ImageIO.read(new File("savanna.png"));
+	            label.setBounds(0,0,800,600);
+	            Image pic4 = pic2.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH);
+	            pic3 = new ImageIcon(pic4);
+	            label.setIcon(pic3);
+	            frame.getContentPane().add(label);
+	            
 			}
-			
-			 catch (IOException e) 
+		
+			catch(IOException a)
 			{
-			    System.out.println("Cannot load image"); 
+				System.out.println("Cant load picture");
 			}
+			*/
 		}
+		
+		if(e.getSource()==green)
+		{
+			//ZooPanel m2=new ZooPanel();
+			/*
+			m.setGreen();
+			m.repaint();
+			m.setVisible(true);
+			//frame.add(m2);
+			frame.setVisible(true);
+			*/
+			frame.remove(label);
+			frame.getContentPane().setBackground(Color.green);
+		}
+		
+		if(e.getSource()==none)
+		{
+			/*
+			m.setNatural();
+			m.repaint();
+			m.setVisible(true);
+			frame.setVisible(true);
+			*/
+			frame.remove(label);
+			frame.getContentPane().setBackground(null);
+		}
+		
+		if(e.getSource()==help2)
+		{
+			JFrame frameMessage=new JFrame("Message");
+			JLabel labelWork2=new JLabel("<html>Home Work 2<br/>GUI</html>");
+			frameMessage.setSize(300,300);
+			frameMessage.setVisible(true);
+			frameMessage.add(labelWork2);
+			ImageIcon aboutIcon=new ImageIcon("about.jpeg");
+			labelWork2.setIcon(aboutIcon);
+			JButton btOk=new JButton("OK");
+			btOk.setFocusable(false);
+			btOk.setBounds(150,150,70,30);
+			labelWork2.add(btOk);	
+			btOk.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e)
+				{  
+					frameMessage.dispose();
+			    }
+			});
+		}
+		
+	}
+		
+		
 }
-
